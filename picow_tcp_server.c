0995bb727e484448742cd6d37760db77f50074b3
Katsumi <51002934+kmorimatsu@users.noreply.github.com>
2023-04-19 16:07:18 -0700

    Simplify tcp server

--- picow_tcp_server.c
+++ picow_tcp_server.c
@@ -19,23 +19,24 @@
 #define TEST_ITERATIONS 10
 #define POLL_TIME_S 5
 
-static const char g_response[]=
+static char g_response[]=
 	"HTTP/1.0 200 OK" "\r\n"
 	"Content-Type: text/html; charset=Shift_JIS" "\r\n"
-	"Content-Length: 92" "\r\n"
+	"Content-Length: 95" "\r\n"
 	"" "\r\n"
 	"<html>"
 	"<head><title>TCP server test</title></head>"
-	"<body>Hello TCP server world!</body>"
+	"<body>Hello TCP server world(0)!</body>"
 	"</html>"
 ;
+static char g_response_num=0x31;
+
+static uint8_t buffer_recv[BUF_SIZE];
 
 typedef struct TCP_SERVER_T_ {
     struct tcp_pcb *server_pcb;
     struct tcp_pcb *client_pcb;
     bool complete;
-    uint8_t buffer_sent[BUF_SIZE];
-    uint8_t buffer_recv[BUF_SIZE];
     int sent_len;
     int recv_len;
     int run_count;
@@ -95,34 +96,21 @@ static err_t tcp_server_sent(void *arg, struct tcp_pcb *tpcb, u16_t len) {
     TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
     DEBUG_printf("tcp_server_sent %u\n", len);
     state->sent_len += len;
-
-    if (state->sent_len >= BUF_SIZE) {
-
-        // We should get the data back from the client
-        state->recv_len = 0;
-        DEBUG_printf("Waiting for buffer from client\n");
-    }
-
     return ERR_OK;
 }
 
 err_t tcp_server_send_data(void *arg, struct tcp_pcb *tpcb)
 {
     TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-    //for(int i=0; i< BUF_SIZE; i++) {
-    for(int i=0; i< (sizeof g_response)-1; i++) {
-        //state->buffer_sent[i] = rand();
-        state->buffer_sent[i] = g_response[i];
-    }
 
+    g_response[(sizeof g_response)-1-17]=g_response_num++;
     state->sent_len = 0;
-    DEBUG_printf("Writing %ld bytes to client\n", BUF_SIZE);
+    DEBUG_printf("Writing %ld bytes to client\n", (sizeof g_response)-1);
     // this method is callback from lwIP, so cyw43_arch_lwip_begin is not required, however you
     // can use this method to cause an assertion in debug mode, if this method is called when
     // cyw43_arch_lwip_begin IS needed
     cyw43_arch_lwip_check();
-    //err_t err = tcp_write(tpcb, state->buffer_sent, BUF_SIZE, TCP_WRITE_FLAG_COPY);
-    err_t err = tcp_write(tpcb, state->buffer_sent, (sizeof g_response)-1, TCP_WRITE_FLAG_COPY);
+    err_t err = tcp_write(tpcb, g_response, (sizeof g_response)-1, TCP_WRITE_FLAG_COPY);
     if (err != ERR_OK) {
         DEBUG_printf("Failed to write data %d\n", err);
         return tcp_server_result(arg, -1);
@@ -134,7 +122,6 @@ err_t tcp_server_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err
     TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
     if (!p) {
     	return tcp_server_client_close(state);
-        //return tcp_server_result(arg, -1);
     }
     // this method is callback from lwIP, so cyw43_arch_lwip_begin is not required, however you
     // can use this method to cause an assertion in debug mode, if this method is called when
@@ -145,34 +132,13 @@ err_t tcp_server_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err
 
         // Receive the buffer
         const uint16_t buffer_left = BUF_SIZE - state->recv_len;
-        state->recv_len += pbuf_copy_partial(p, state->buffer_recv + state->recv_len,
+        state->recv_len += pbuf_copy_partial(p, buffer_recv + state->recv_len,
                                              p->tot_len > buffer_left ? buffer_left : p->tot_len, 0);
         tcp_recved(tpcb, p->tot_len);
     }
     pbuf_free(p);
 
-    // Have we have received the whole buffer
-    /*if (state->recv_len == BUF_SIZE) {
-
-        // check it matches
-        if (memcmp(state->buffer_sent, state->buffer_recv, BUF_SIZE) != 0) {
-            DEBUG_printf("buffer mismatch\n");
-            return tcp_server_result(arg, -1);
-        }
-        DEBUG_printf("tcp_server_recv buffer ok\n");
-
-        // Test complete?
-        state->run_count++;
-        if (state->run_count >= TEST_ITERATIONS) {
-            tcp_server_result(arg, 0);
-            return ERR_OK;
-        }
-
-        // Send another buffer
-        return tcp_server_send_data(arg, state->client_pcb);
-    }//*/
     return tcp_server_send_data(arg, state->client_pcb);
-    return ERR_OK;
 }
 
 static err_t tcp_server_poll(void *arg, struct tcp_pcb *tpcb) {
@@ -249,45 +215,7 @@ void run_tcp_server_test(void) {
         return;
     }
     while(!state->complete) {
-        // the following #ifdef is only here so this same example can be used in multiple modes;
-        // you do not need it in your code
-#if PICO_CYW43_ARCH_POLL
-        // if you are using pico_cyw43_arch_poll, then you must poll periodically from your
-        // main loop (not from a timer) to check for Wi-Fi driver or lwIP work that needs to be done.
-        cyw43_arch_poll();
-        // you can poll as often as you like, however if you have nothing else to do you can
-        // choose to sleep until either a specified time, or cyw43_arch_poll() has work to do:
-        cyw43_arch_wait_for_work_until(make_timeout_time_ms(1000));
-#else
-        // if you are not using pico_cyw43_arch_poll, then WiFI driver and lwIP work
-        // is done via interrupt in the background. This sleep is just an example of some (blocking)
-        // work you might be doing.
         sleep_ms(1000);
-#endif
     }
     free(state);
 }
-
-/*
-int main() {
-    stdio_init_all();
-
-    if (cyw43_arch_init()) {
-        printf("failed to initialise\n");
-        return 1;
-    }
-
-    cyw43_arch_enable_sta_mode();
-
-    printf("Connecting to Wi-Fi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
-        printf("failed to connect.\n");
-        return 1;
-    } else {
-        printf("Connected.\n");
-    }
-    run_tcp_server_test();
-    cyw43_arch_deinit();
-    return 0;
-}
-//*/
